c47b221056d3999dcb42d93f71d5f3204629e944
tcpaul <tcpaul9@gmail.com>
2018-06-25 18:02:45 +0200

    successful post to slack webhook

--- server.js
+++ server.js
@@ -4,6 +4,25 @@ var app = express();
 
 var base_URL = 'https://api.eos.bitspace.no/v1/chain/';
 
+var last_block = null;
+
+var _POST = function(URL, jsonData) {
+  request.post(
+    {
+      url: URL,
+      method: 'POST',
+      json: jsonData
+    },
+    function(error, response, body) {
+      if (!error) {
+        console.log(body);
+      } else {
+        console.log(error);
+      }
+    }
+  );
+};
+
 var blockchain_freeze = setInterval(function() {
   request(
     {
@@ -14,11 +33,19 @@ var blockchain_freeze = setInterval(function() {
       maxRedirects: 10
     },
     function(error, response, body) {
+      var block_header = body;
       if (!error && response.statusCode == 200) {
-        console.log('success!' + body);
+        if (last_block !== null && last_block !== block_header) {
+          console.log('blockchain is frozen! ' + body);
+          _POST(
+            'https://hooks.slack.com/services/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',
+            { text: 'it works!' }
+          );
+        }
       } else {
         console.log('error' + response.statusCode);
       }
+      last_block = block_header;
     }
   );
 }, 60000);
